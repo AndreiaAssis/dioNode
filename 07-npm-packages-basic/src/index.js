9b2746d73eeaee3b06d9258893e00a9bf47dbb8f
Andreia Araújo De Assis <13038693650@todos.net>
2024-07-22 16:45:14 -0300

    conclusão projeto 07-npm-packages-basic

---
+++ 07-npm-packages-basic/src/index.js
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+import logSymbols from "log-symbols";
+
+//console.log(chalk.blue.bgGray.bold("Hello Word!"));
+
+//console.log(chalk.blue.bgGreen.italic("Hello Word!"));
+
+console.log(logSymbols.success, "Servidor iniciado com sucesso");
