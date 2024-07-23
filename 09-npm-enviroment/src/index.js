57d828e4883dcc2017dcc734bd338ea62ec74257
Andreia Araújo De Assis <13038693650@todos.net>
2024-07-23 17:37:52 -0300

    '09-npm-enviroment

---
+++ 09-npm-enviroment/src/index.js
@@ -0,0 +1,9 @@
+async function connectToDatabase(user, password){
+    if(user === "naruto" && password === "uzumaki"){
+        console.log("conexão com banco de dados estabelecida.");
+    }else{
+        console.log("Falha de login, não foi possível se conectar ao banco de dados.");
+    }
+}
+
+export default connectToDatabase;
