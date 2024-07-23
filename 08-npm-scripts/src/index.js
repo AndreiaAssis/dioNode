71469b03b0006323d0d8fb45041b601684733c67
Andreia Araújo De Assis <13038693650@todos.net>
2024-07-23 16:22:58 -0300

    08-npm-scripts

---
+++ 08-npm-scripts/src/index.js
@@ -0,0 +1,5 @@
+console.log("hello world");
+console.log("Iniciando Servidor");
+
+//script package.json:  // "test": "echo \"Error: no test specified\" && exit 1",
+    // "iniciar" : "node src/index.js && mkdir dist && echo \" pasta dist criada \" "
